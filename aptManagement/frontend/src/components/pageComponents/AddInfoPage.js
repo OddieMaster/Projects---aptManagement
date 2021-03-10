47b32e6c4156367659e146c2f62087a88b54e353
Oddie <romualdo.gui@gmail.com>
2021-03-10 16:08:49 -0300

    Fix select from AddInfoPage component, validade with form hook

--- aptManagement/frontend/src/components/pageComponents/AddInfoPage.js
+++ aptManagement/frontend/src/components/pageComponents/AddInfoPage.js
@@ -6,7 +6,12 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import AddCircleIcon from '@material-ui/icons/AddCircle';
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import { useForm, Controller } from "react-hook-form";
 
 const useStyles = makeStyles((theme) => ({
   input: {
@@ -16,12 +21,26 @@ const useStyles = makeStyles((theme) => ({
   button: {
     marginTop: theme.spacing(4),
     backgroundColor: "#22bb33",
-    color: "white"
+    color: "white",
+  },
+  formControl: {
+    minWidth: 120,
+    width: 300,
+  },
+  error: {
+    color: "red",
+  },
+  submit: {
+    backgroundColor: "#6CB26C",
+    margin: theme.spacing(3, 0, 2),
+    color: "#ffffff",
+    fontWeight: "bold",
   },
 }));
 
 const AddInfoPage = (props) => {
   const [open, setOpen] = React.useState(false);
+  const { handleSubmit, register, control, errors } = useForm({});
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -31,39 +50,21 @@ const AddInfoPage = (props) => {
     setOpen(false);
   };
 
-  const [apartment, setAparment] = useState([]);
-  const [block, setBlock] = useState([]);
-  const [resident, setResident] = useState([]);
+  function onSubmit(formData) {
+    console.log(formData);
+  }
 
   const classes = useStyles();
 
-  function handleChangeApartment(event) {
-    let textAp = event.target.value;
-    console.log({ textAp });
-    setAparment(textAp);
-  }
-
-  function handleChangeBlock(event) {
-    let textBlock = event.target.value;
-    console.log({ textBlock });
-    setBlock(textBlock);
-  }
-
-  function handleChangeResident(event) {
-    let textDw = event.target.value;
-    console.log({ textDw });
-    setResident(textDw);
-  }
-
-  function handleClickFinish() {
-    props.addItem(apartment, block, resident);
-    console.log(apartment)
-    setOpen(false);
-  }
-
   return (
     <>
-      <Button variant="outlined" color="primary" className={classes.button} startIcon={<AddCircleIcon />} onClick={handleClickOpen}>
+      <Button
+        variant="outlined"
+        color="primary"
+        className={classes.button}
+        startIcon={<AddCircleIcon />}
+        onClick={handleClickOpen}
+      >
         {props.title}
       </Button>
       <Dialog
@@ -71,54 +72,87 @@ const AddInfoPage = (props) => {
         onClose={handleClose}
         aria-labelledby="form-dialog-title"
       >
-        <DialogTitle id="form-dialog-title">Register Information</DialogTitle>
-        <DialogContent>
-          <p>Apartment Number</p>
+        <DialogTitle id="form-dialog-title">
+          Link apt and block to resident
+        </DialogTitle>
+        <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
+          <DialogContent>
+            <p>Apartment Number</p>
 
-          <Input
-            className={classes.input}
-            autoFocus
-            id="aparment"
-            value={apartment}
-            onChange={handleChangeApartment}
-          />
+            <Input
+              className={classes.input}
+              autoFocus
+              id="apartment"
+              name="apartment"
+              inputRef={register({
+                required: true,
+              })}
+            />
+            {errors.apartment && errors.apartment.type === "required" && (
+              <p className={classes.error}>Invalid Apartment</p>
+            )}
 
-          <p>Block</p>
+            <p>Block</p>
 
-          <Input
-            className={classes.input}
-            id="block"
-            value={block}
-            onChange={handleChangeBlock}
-          />
-          <p> Resident</p>
-          <Input
-            className={classes.input}
-            id="resident"
-            value={resident}
-            onChange={handleChangeResident}
-            
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button
-            fullWidth
-            type="submit"
-            variant="contained"
-            color="primary"
-            onClick={handleClickFinish}
-          >
-            Submit
-          </Button>
-          <Button
-            fullWidth
-            variant="contained"
-            onClick={handleClose}
-            color="primary"
-          >
-            Cancel
-          </Button>
-        </DialogActions>
+            <Input
+              className={classes.input}
+              id="block"
+              name="block"
+              inputRef={register({
+                required: true,
+              })}
+            />
+            {errors.block && errors.block.type === "required" && (
+              <p className={classes.error}>Invalid Block</p>
+            )}
+            <br />
+            <FormControl className={classes.formControl}>
+              <InputLabel id="resident">Resident</InputLabel>
+              <Controller
+                as={
+                  <Select
+                    labelId="selectResident"
+                    id="selectResident"
+                    name="resident"
+                    defaultValue=""
+                  >
+                    {props.data.map((row) => (
+                      <MenuItem value={row.resident || ""} key={row.id}>
+                        {row.resident}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                }
+                name="Select"
+                control={control}
+                rules={{
+                  required: true,
+                }}
+                defaultValue=""
+              />
+            </FormControl>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              fullWidth
+              className={classes.submit}
+              type="submit"
+              variant="contained"
+              color="inherit"
+            >
+              Submit
+            </Button>
+            <Button
+              className={classes.submit}
+              fullWidth
+              variant="contained"
+              onClick={handleClose}
+              color="inherit"
+            >
+              Cancel
+            </Button>
+          </DialogActions>
+        </form>
       </Dialog>
     </>
   );
